e818ae7eef8fe3d757dcf32e1cc06b0633b217ac
Silja23 <haeslsil@students.zhaw.ch>
2026-03-21 10:50:51 +0100

    secrets angepasst

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
 pg_second = st.Page("views/Budget_rechner.py", title="Budget-Rechner", icon=":material/info:")
 
+
 pg = st.navigation([pg_home, pg_second])
 pg.run()
 
